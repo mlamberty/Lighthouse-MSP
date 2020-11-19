b893fb2b59d07684521a0395ff99f0d1c9ac89b3
mlamberty <74625538+mlamberty@users.noreply.github.com>
2020-11-19 17:56:23 -0600

    Added deployment templates and PS script

---
+++ Deployment/Deploy-DelegatedManagement.ps1
@@ -0,0 +1,9 @@
+﻿# Log in first with Connect-AzAccount if you're not using Cloud Shell
+Connect-AzAccount
+# Deploy Azure Resource Manager template using template and parameter file locally
+New-AzSubscriptionDeployment -Name Synnex-Lighthouse `
+                 -Location eastus `
+                 -TemplateFile C:\Users\matthewl\Downloads\Azure\WVD\rgdelegatedResourceManagement.json `
+                 -TemplateParameterFile C:\Users\matthewl\Downloads\Azure\WVD\rgdelegatedResourceManagement.parameters.json `
+                 -Verbose
+
