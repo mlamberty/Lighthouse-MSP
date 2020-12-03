481d8edce02b387c298a3fae0a7e9aa8a5512dd4
mlamberty <74625538+mlamberty@users.noreply.github.com>
2020-12-03 14:49:48 -0600

    workspace key property

--- Workspace/Deploy-WOrkspace.ps1
+++ Workspace/Deploy-WOrkspace.ps1
@@ -4,7 +4,7 @@ Connect-AzAccount -Tenant c01f33e1-d38a-47c8-a733-af070bcc674d -Subscription "Pa
 # Deploy Azure Resource Manager template using template and parameter file locally
 New-AzResourceGroupDeployment -Name workspace `
                  -ResourceGroupName Lighthouse-test `
-                 -TemplateUri https://raw.githubusercontent.com/mlamberty/Lighthouse-MSP/main/Workspace/template.json `
-                 -TemplateParameterUri https://raw.githubusercontent.com/mlamberty/Lighthouse-MSP/main/Workspace/parameters.json `
+                 -TemplateUri https://raw.githubusercontent.com/mlamberty/Lighthouse-MSP/main/Workspace/workspaceconfig.json `
+                 -WhatIf
                  -Verbose -DeploymentDebugLogLevel All 
 
