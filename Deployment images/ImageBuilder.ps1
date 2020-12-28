2fcfcf94fb6b06948a548c0d4e64c5b687cff765
StellrMSP <75386725+StellrMSP@users.noreply.github.com>
2020-12-28 17:45:46 -0600

    Add files via upload

---
+++ Deployment images/ImageBuilder.ps1
@@ -0,0 +1,143 @@
+# Step 1: Connect
+Connect-AzAccount
+Get-AzSubscription | Out-GridView -PassThru | Select-AzSubscription
+
+# Step 2: get existing context
+$currentAzContext = Get-AzContext
+
+# destination image resource group
+$imageResourceGroup = "myimagebuilder-rg"
+
+# location (see possible locations in main docs)
+$location = "westeurope"
+
+## if you need to change your subscription: Get-AzSubscription / Select-AzSubscription -SubscriptionName 
+
+# get subscription, this will get your current subscription
+$subscriptionID = $currentAzContext.Subscription.Id
+
+# name of the image to be created
+$imageName = "Win1020h1"
+
+# image distribution metadata reference name
+$runOutputName = "win1020h1ManImg01ro"
+
+# image template name
+$imageTemplateName = "window1020h1Template01"
+
+# distribution properties object name (runOutput), i.e. this gives you the properties of the managed image on completion
+$runOutputName = "winSvrSigR01"
+
+# create resource group for image and image template resource
+if ($null -eq (Get-AzResourceGroup -Name $imageResourceGroup -ErrorAction SilentlyContinue)) {
+    New-AzResourceGroup -Name $imageResourceGroup -Location $location
+}
+
+# setup role def names, these need to be unique
+$imageRoleDefName = "Azure Image Builder Image Def"
+$idenityName = "aibIdentity"
+
+## Add AZ PS module to support AzUserAssignedIdentity
+if ($null -eq (Get-Module -ListAvailable Az.ManagedServiceIdentity)) {
+    Install-Module -Name Az.ManagedServiceIdentity
+}
+
+# create identity
+if ($null -eq (Get-AzUserAssignedIdentity -Name $idenityName -ResourceGroupName $imageResourceGroup -ErrorAction SilentlyContinue)) {
+    New-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName
+}
+
+$idenityNameResourceId = $(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).Id
+$idenityNamePrincipalId = $(Get-AzUserAssignedIdentity -ResourceGroupName $imageResourceGroup -Name $idenityName).PrincipalId
+
+#Check Custom Role definition
+if ($null -eq (Get-AzRoleDefinition -Name $imageRoleDefName -ErrorAction SilentlyContinue)) {
+    #Custom Role definition does not exist. Creating now...
+    $aibRoleImageCreationUrl = "https://raw.githubusercontent.com/danielsollondon/azvmimagebuilder/master/solutions/12_Creating_AIB_Security_Roles/aibRoleImageCreation.json"
+    $aibRoleImageCreationPath = "aibRoleImageCreation.json"
+
+    # download config
+    Invoke-WebRequest -Uri $aibRoleImageCreationUrl -OutFile $aibRoleImageCreationPath -UseBasicParsing
+
+    ((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<subscriptionID>', $subscriptionID) | Set-Content -Path $aibRoleImageCreationPath
+    ((Get-Content -path $aibRoleImageCreationPath -Raw) -replace '<rgName>', $imageResourceGroup) | Set-Content -Path $aibRoleImageCreationPath
+    ((Get-Content -path $aibRoleImageCreationPath -Raw) -replace 'Azure Image Builder Service Image Creation Role', $imageRoleDefName) | Set-Content -Path $aibRoleImageCreationPath
+
+    # create role definition
+    New-AzRoleDefinition -InputFile  ./aibRoleImageCreation.json
+    ### NOTE: If you see this error: 'New-AzRoleDefinition: Role definition limit exceeded. No more role definitions can be created.' See this article to resolve: https://docs.microsoft.com/en-us/azure/role-based-access-control/troubleshooting
+}
+
+#Check Role Assignment
+if ($null -eq (Get-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup" -ErrorAction SilentlyContinue)) {
+    # grant role definition to image builder service principal
+    New-AzRoleAssignment -ObjectId $idenityNamePrincipalId -RoleDefinitionName $imageRoleDefName -Scope "/subscriptions/$subscriptionID/resourceGroups/$imageResourceGroup"
+}
+
+
+# update AIB image config template
+$templateUrl = "https://raw.githubusercontent.com/michawets/AzureImageBuilderPOC/master/clean_template_win1020h1.json"
+$templateFilePath = "helloImageTemplateWin01.json"
+
+# download configs
+Invoke-WebRequest -Uri $templateUrl -OutFile $templateFilePath -UseBasicParsing
+
+((Get-Content -path $templateFilePath -Raw) -replace '<subscriptionID>', $subscriptionID) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<rgName>', $imageResourceGroup) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<region>', $location) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<runOutputName>', $runOutputName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<imageName>', $imageName) | Set-Content -Path $templateFilePath
+((Get-Content -path $templateFilePath -Raw) -replace '<imgBuilderId>', $idenityNameResourceId) | Set-Content -Path $templateFilePath
+
+New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -api-version "2019-05-01-preview" -imageTemplateName $imageTemplateName -svclocation $location
+
+# note this will take minute, as validation is run (security / dependencies etc.)
+
+Invoke-AzResourceAction -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2020-02-14" -Action Run -Force
+
+########################################
+#Get Status of the Image Build and Query
+$resourcetowatch = Get-AzResource –ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $imageTemplateName
+do {
+    $status = (Get-AzResource –ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -Name $imageTemplateName).Properties.lastRunStatus
+    $status | Format-Table *
+    Start-Sleep -Seconds 30
+} while ($status.runState -eq "Running")
+
+
+##Cancel build
+Invoke-AzResourceAction -ResourceName $imageTemplateName -ResourceGroupName $imageResourceGroup -ResourceType Microsoft.VirtualMachineImages/imageTemplates -ApiVersion "2020-02-14" -Action Cancel -Force
+
+
+##Create a VM
+$imageResourceGroup = "myResourceGroup"
+$vmName = "aibVm1"
+
+# Create user object
+$cred = Get-Credential -Message "Enter a username and password for the virtual machine."
+
+# Create a resource group
+New-AzResourceGroup -Name $imageResourceGroup -Location $location
+
+# Network pieces
+$subnetConfig = New-AzVirtualNetworkSubnetConfig -Name mySubnet -AddressPrefix 192.168.1.0/24
+$vnet = New-AzVirtualNetwork -ResourceGroupName $imageResourceGroup -Location $location `
+    -Name MYvNET -AddressPrefix 192.168.0.0/16 -Subnet $subnetConfig
+$pip = New-AzPublicIpAddress -ResourceGroupName $imageResourceGroup -Location $location `
+    -Name "mypublicdns$(Get-Random)" -AllocationMethod Static -IdleTimeoutInMinutes 4
+$nsgRuleRDP = New-AzNetworkSecurityRuleConfig -Name myNetworkSecurityGroupRuleRDP  -Protocol Tcp `
+    -Direction Inbound -Priority 1000 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * `
+    -DestinationPortRange 3389 -Access Allow
+$nsg = New-AzNetworkSecurityGroup -ResourceGroupName $imageResourceGroup -Location $location `
+    -Name myNetworkSecurityGroup -SecurityRules $nsgRuleRDP
+$nic = New-AzNetworkInterface -Name myNic -ResourceGroupName $imageResourceGroup -Location $location `
+    -SubnetId $vnet.Subnets[0].Id -PublicIpAddressId $pip.Id -NetworkSecurityGroupId $nsg.Id
+
+# Create a virtual machine configuration using $imageVersion.Id to specify the shared image
+$vmConfig = New-AzVMConfig -VMName $vmName -VMSize Standard_D1_v2 | `
+    Set-AzVMOperatingSystem -Windows -ComputerName $vmName -Credential $cred | `
+    Set-AzVMSourceImage -Id $imageVersion.Id | `
+    Add-AzVMNetworkInterface -Id $nic.Id
+
+# Create a virtual machine
+New-AzVM -ResourceGroupName $imageResourceGroup -Location $location -VM $vmConfig
--- Deployment images/install_apps.ps1
+++ Deployment images/install_apps.ps1
@@ -8,51 +8,14 @@
 # Image customization
 # https://minhaskamal.github.io/DownGit/#/home?url=https://github.com/shawntmeyer/WVD/tree/master/Image-Build/Customizations
 
-# Allow Sripts
-Set-ExecutionPolicy -ExecutionPolicy Unrestricted  -Scope LocalMachine -Force
+New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
 
 
+Invoke-WebRequest -Uri https://github.com/mlamberty/Lighthouse-MSP/tree/main/Deployment%20images/Apps.zip -OutFile C:\temp\Apps.zip
+Expand-Archive -LiteralPathC:\temp\Apps.zip -DestinationPath C:\temp
+Invoke-Expression -Command 'c:\temp\Apps\VScodeSetup-x64-1.52.1.exe /verysilent'
+Invoke-Expression -Command 'c:\temp\Apps\readerdc_en_ka_cra_install.exe /verysilent'
+Invoke-Expression -Command 'c:\temp\Apps\npp.7.8.8.Installer.exe /verysilent'
+Invoke-Expression -Command 'c:\temp\Apps\ChromeSetup.exe /verysilent'
 
-# Set Logging 
-$logFile = "c:\temp\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
-function Write-Log {
-    Param($message)
-    Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
-}
-#end
-
-# Putty Install
-try {
-    Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\putty-64bit-0.74-installer.msi', '/quiet', 'ADDLOCAL="Putty"'
-    if (Test-Path "C:\Program Files\PuTTY\putty.exe") {
-        Write-Log "Putty has been installed"
-    }
-    else {
-        write-log "Error locating the Putty executable"
-    }
-}
-catch {
-    $ErrorMessage = $_.Exception.message
-    write-log "Error installing Putty: $ErrorMessage"
-}
-#end
-
-# Notepad++ Install
-try {
-    Start-Process -filepath 'c:\temp\npp.7.8.8.Installer.x64.exe' -Wait -ErrorAction Stop -ArgumentList '/S'
-    Copy-Item 'C:\temp\config.model.xml' 'C:\Program Files\Notepad++'
-    Rename-Item 'C:\Program Files\Notepad++\updater' 'C:\Program Files\Notepad++\updaterOld'
-    if (Test-Path "C:\Program Files\Notepad++\notepad++.exe") {
-        Write-Log "Notepad++ has been installed"
-    }
-    else {
-        write-log "Error locating the Notepad++ executable"
-    }
-}
-catch {
-    $ErrorMessage = $_.Exception.message
-    write-log "Error installing Notepad++: $ErrorMessage"
-}
-#end
-
-
+ 
